f8e02997c3affbc0301e4b30b87e4426c850b041
brookmezgu <brookmezgebu64@gmail.com>
2020-03-23 22:39:16 +0300

    detail_screen: add detail screen

---
+++ app/src/main/java/ethiopia/covid/android/ui/fragment/DetailFragment.java
@@ -0,0 +1,38 @@
+package ethiopia.covid.android.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import ethiopia.covid.android.R;
+
+/**
+ * Created by BrookMG on 3/23/2020 in ethiopia.covid.android.ui.fragment
+ * inside the project CoVidEt .
+ */
+public class DetailFragment extends BaseFragment {
+
+    private RecyclerView recyclerView;
+
+    public static DetailFragment newInstance() {
+        Bundle args = new Bundle();
+        DetailFragment fragment = new DetailFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View mainView = inflater.inflate(R.layout.blank_fragment, container, false);
+        recyclerView = mainView.findViewById(R.id.recycler_view);
+        return mainView;
+    }
+
+}
