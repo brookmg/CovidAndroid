81de092452add251fdc917c1737a44680fb15055
brookmezgu <brookmezgebu64@gmail.com>
2020-03-23 15:19:34 +0300

    codebase: organise classes

--- app/src/main/java/ethiopia/covid/android/ui/activity/MainActivity.java
+++ app/src/main/java/ethiopia/covid/android/ui/activity/MainActivity.java
@@ -1,8 +1,9 @@
-package ethiopia.covid.android;
+package ethiopia.covid.android.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import ethiopia.covid.android.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
