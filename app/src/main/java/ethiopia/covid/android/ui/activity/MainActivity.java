5ce8b3c37353bd87f9bf273eef4e76cadf33a7f0
brookmezgu <brookmezgebu64@gmail.com>
2020-03-23 17:37:57 +0300

    codebase: decouple activity from screens, reuse a single activity

--- app/src/main/java/ethiopia/covid/android/ui/activity/MainActivity.java
+++ app/src/main/java/ethiopia/covid/android/ui/activity/MainActivity.java
@@ -1,17 +1,61 @@
 package ethiopia.covid.android.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.lang.ref.WeakReference;
+
 import ethiopia.covid.android.R;
+import ethiopia.covid.android.ui.fragment.BaseFragment;
+import ethiopia.covid.android.ui.fragment.HomeFragment;
+
+import static ethiopia.covid.android.util.Constant.TAG_HOME;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout _fragmentContainer;
+    private WeakReference<Fragment> currentFragment;
+
+    private void setCurrentFragment(BaseFragment fragment) {
+        currentFragment = new WeakReference<>(fragment);
+    }
+
+    private void fragStarter (String fragTag , BaseFragment baseFragment , Bundle bundle, View sharedView) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer , baseFragment , fragTag);
+        if (sharedView != null){
+            String transitionName = ViewCompat.getTransitionName(sharedView);
+            fragmentTransaction.addSharedElement(sharedView , transitionName != null ? transitionName : "nothing_here");
+        }
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        setCurrentFragment(baseFragment);
+    }
+
+    public void changeFragment (String fragmentTag , Bundle bundle, @Nullable View view) {
+        switch (fragmentTag) {
+            case TAG_HOME: {
+                BaseFragment baseFragment = HomeFragment.newInstance();
+                fragStarter(fragmentTag , baseFragment , bundle, view);
+                break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        _fragmentContainer = findViewById(R.id.fragmentContainer);
+        changeFragment(TAG_HOME, new Bundle(), null);
 
     }
 }
---
+++ app/src/main/java/ethiopia/covid/android/util/Constant.java
@@ -0,0 +1,11 @@
+package ethiopia.covid.android.util;
+
+/**
+ * Created by BrookMG on 3/23/2020 in ethiopia.covid.android.util
+ * inside the project CoVidEt .
+ */
+public class Constant {
+
+    public static final String TAG_HOME = "tag_home";
+
+}
